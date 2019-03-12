a0e5b9a7977e2fc6aa30c47d0f5511c8c840fffe
Akshat Khare <cs1160315@iitd.ac.in>
2019-03-13 02:52:43 +0530

    tested on the pdf dataset

--- ass2queryhelp.sql
+++ ass2queryhelp.sql
@@ -8,6 +8,9 @@ Arrival_Time time
 );
 copy TrainSchedule(Train_ID, Source, Destination, Distance, Departure_Time, Arrival_Time) from '/home/codebat/Documents/codes/col362/ass2/raildata20.csv' DELIMITER ',' CSV HEADER;
 
+copy TrainSchedule(Train_ID, Source, Destination, Distance, Departure_Time, Arrival_Time) from '/home/codebat/Documents/codes/col362/ass2/pdfraildata.csv' DELIMITER '|' CSV HEADER;
+
+
 create view edge as select distinct Source, Destination from TrainSchedule;
 
 create view path as 
@@ -39,3 +42,27 @@ SELECT TrainSchedule.Source, Path.Destination
 FROM Path, TrainSchedule
 WHERE TrainSchedule.Destination = Path.Source)
 SELECT * FROM Path;
+
+
+
+--NOT USED--
+create view pathdetail as
+WITH RECURSIVE Path (Source,Destination, TimeSpent, Distance) AS
+(SELECT Source, Destination, Arrival_Time - Departure_Time as TimeSpent, Distance
+FROM TrainSchedule
+UNION
+SELECT TrainSchedule.Source, Path.Destination, Path.TimeSpent + TrainSchedule.Arrival_Time-TrainSchedule.Departure_Time as TimeSpent, Path.Distance + TrainSchedule.Distance as Distance
+FROM Path, TrainSchedule
+WHERE TrainSchedule.Destination = Path.Source)
+SELECT * FROM Path;
+
+create view pathq2 as 
+WITH RECURSIVE Path (Source,Destination) AS
+(SELECT Source, Destination
+FROM edgelessthanonehour
+UNION
+SELECT edgelessthanonehour.Source, Path.Destination
+FROM Path, edgelessthanonehour
+WHERE edgelessthanonehour.Destination = Path.Source)
+SELECT * FROM Path;
+--NOT USED--
